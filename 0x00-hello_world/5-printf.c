9f32090791eb0783a8222d598b0352fde127ff06
mohamed embabi <137911793+M-EMBABI@users.noreply.github.com>
2023-10-05 21:26:53 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- * Return: Always return 0 (Success)
+ * main - Entry of the program
+ * Return: Always returns (0)
  */
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
